46a42cacc83f9a9b3b72d61f92aedd13d06a8a67
JuanDev <juniorguzman02@hotmail.com>
2023-03-01 19:11:55 -0600

    Displaying API info

--- recipe-app/src/components/Trending.jsx
+++ recipe-app/src/components/Trending.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Trending = () => {
+  const [trending, setTrending] = useState([]);
+
   useEffect(() => {
     getTrending();
   }, []);
@@ -10,10 +12,26 @@ const Trending = () => {
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOON_KEY}&number=10`
     );
     const data = await api.json();
-    console.log(data);
+    console.log(data.recipes);
+    setTrending(data.recipes);
   };
 
-  return <div>Trending</div>;
+  return (
+    <div>
+      {trending.map((recipe) => {
+        return (
+          <section className='wrapper' key={recipe.id}>
+            <div className='recipe-card'>
+              <img src={recipe.image} alt='' />
+              <p>{recipe.title}</p>
+              <p>{recipe.readyInMinutes}</p>
+              <p>{recipe.servings}</p>
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Trending;
