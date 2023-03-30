95abef2be5e44a0de8cf7bcf7d286515c9fe6949
JuanDev <juniorguzman02@hotmail.com>
2023-03-29 19:33:10 -0500

    finished styling

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import Category from './components/Category';
 import Pages from './pages/Pages';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Link} from "react-router-dom"
 import Search  from './components/Search';
+import styled from 'styled-components';
+import {TbBowl} from 'react-icons/tb'
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+          <Nav>
+            <TbBowl /> 
+            <Logo to={'/'}>GoodEats</Logo>
+          </Nav>
           <Search/>
           <Category/>  
           <Pages/>
@@ -16,5 +23,24 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+text-decoration: none;
+font-size: 1.5rem ;
+font-weight: 400;
+font-family: 'Lobster Two',cursive;
+
+`
+
+const Nav = styled.div`
+padding: 4rem 0rem;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+
+svg{
+  font-size: 2rem;
+}
+`
 
 export default App;
--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Trending from "../components/Trending";
 import Veggie from "../components/Veggie";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <section>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <Trending />
       <Veggie />
-    </section>
+    </motion.div>
   );
 };
 
